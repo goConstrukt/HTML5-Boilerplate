46d3860ee9d6528b7c7dd7bb0b36cb0143a45c6a
Kevin Perrine <kperrine@gmail.com>
2012-01-03 16:49:57 -0800

    Added the less rhino compiler and modified the buildscript to use it before compressing css

--- build/runbuildscript.bat
+++ build/runbuildscript.bat
@@ -3,4 +3,5 @@ REM # If you're on a mac or linux, just run `ant build` from this folder in Term
 
 set MYDIR=%~dp0
 set ANT_OPTS=-D"file.encoding=UTF-8"
+java -jar ./tools/rhino.jar ./tools/less-rhino-1.1.5.js ../less/style.less ../css/style.css
 ant build
