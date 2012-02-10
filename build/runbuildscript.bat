b810b2ae9729612cc88b91b298404c072114bacf
Kevin Perrine <kperrine@gmail.com>
2012-02-09 16:58:19 -0800

    merged upstream changes

--- build/runbuildscript.bat
+++
@@ -1,7 +0,0 @@
-REM # This is for windows users only.
-REM # If you're on a mac or linux, just run `ant build` from this folder in Terminal
-
-set MYDIR=%~dp0
-set ANT_OPTS=-D"file.encoding=UTF-8"
-java -jar ./tools/rhino.jar ./tools/less-rhino-1.1.5.js ../less/style.less ../css/style.css
-ant build
